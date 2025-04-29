31a08bd889748a1a034a42e5d925ded76a67d07a
Ansh Arora <ansha4179@gmail.com>
2025-04-29 20:54:52 +0530

    feat: new changes

--- src/components/BodyComponent.tsx
+++ src/components/BodyComponent.tsx
@@ -1,5 +1,6 @@
 import BottomSection from "./BottomSection";
 import CaseStudyCarousel from "./CaseStudyCarousel";
+import GlobalClickSpark from "./GlobalClickSpark";
 import HeroText from "./heroText";
 import PartnersSection from "./PartnersSection";
 import BoughServices from "./Services";
@@ -7,11 +8,13 @@ import BoughServices from "./Services";
 export default function BodyComponent() {
   return (
     <div className="bg-[url('/body.svg')]  bg-center h-full w-full">
-      <HeroText />
-      <BoughServices />
-      <CaseStudyCarousel />
-      <PartnersSection />
-      <BottomSection />
+      <GlobalClickSpark>
+        <HeroText />
+        <BoughServices />
+        <CaseStudyCarousel />
+        <PartnersSection />
+        <BottomSection />
+      </GlobalClickSpark>
     </div>
   );
 }
