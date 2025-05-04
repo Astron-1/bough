685cb70ce6b215734a05529fc1ae57d5025ab35d
Ansh Arora <ansha4179@gmail.com>
2025-05-04 16:09:26 +0530

    fix: design upgrades and figma based changes

--- src/components/about/WhoWeAreSection.tsx
+++ src/components/about/WhoWeAreSection.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Text, { Font } from "@app/components/Text";
-import { createResponsiveVars, figmaSectionContainer } from "@app/utils/figmaUtils";
+import {
+  createResponsiveVars,
+  figmaSectionContainer,
+} from "@app/utils/figmaUtils";
 
 interface TeamPhotoProps {
   alt: string;
@@ -16,8 +19,14 @@ interface WhoWeAreSectionProps {
   photos: TeamPhotoProps[];
 }
 
-const TeamPhoto = ({ alt, src, placeholderText, className, style }: TeamPhotoProps) => (
-  <div 
+const TeamPhoto = ({
+  alt,
+  src,
+  placeholderText,
+  className,
+  style,
+}: TeamPhotoProps) => (
+  <div
     className={`bg-gray-200 rounded-lg overflow-hidden shadow-sm hover:shadow-md transition-shadow duration-300 ${className}`}
     style={style}
   >
@@ -31,7 +40,11 @@ const TeamPhoto = ({ alt, src, placeholderText, className, style }: TeamPhotoPro
   </div>
 );
 
-export default function WhoWeAreSection({ title, paragraphs, photos }: WhoWeAreSectionProps) {
+export default function WhoWeAreSection({
+  title,
+  paragraphs,
+  photos,
+}: WhoWeAreSectionProps) {
   // Define base dimensions from Figma
   const bentoGridDimensions = {
     grid1Width: "12.77344rem",
@@ -47,14 +60,18 @@ export default function WhoWeAreSection({ title, paragraphs, photos }: WhoWeAreS
     grid6Width: "8.08713rem",
     grid6Height: "6.97394rem",
     spacingH: "0.85rem",
-    spacingV: "0.85rem"
+    spacingV: "0.85rem",
   };
 
   // Generate CSS variables for all screen sizes
   const responsiveVars = createResponsiveVars(bentoGridDimensions);
 
   return (
-    <div className="relative z-10 py-12 md:py-16 px-4 sm:px-6" style={figmaSectionContainer()}>
+    <div
+      id="who-we-are"
+      className="relative z-10 py-12 md:py-16 px-4 sm:px-6"
+      style={figmaSectionContainer()}
+    >
       <div className="grid grid-cols-1 md:grid-cols-12 gap-10 lg:gap-20">
         {/* Left side - Bento grid layout for photos */}
         <div className="relative md:col-span-5 flex justify-center md:justify-start">
@@ -74,92 +91,131 @@ export default function WhoWeAreSection({ title, paragraphs, photos }: WhoWeAreS
                   alt={photos[0]?.alt || "Team Photo 1"}
                   src={photos[0]?.src}
                   placeholderText={photos[0]?.placeholderText}
-                  style={{ width: "var(--grid1Width)", height: "var(--grid1Height)" }}
+                  style={{
+                    width: "var(--grid1Width)",
+                    height: "var(--grid1Height)",
+                  }}
                 />
               </div>
 
               {/* Top row - right image */}
-              <div className="absolute top-0" style={{ left: "calc(var(--grid1Width) + var(--spacingH))" }}>
+              <div
+                className="absolute top-0"
+                style={{ left: "calc(var(--grid1Width) + var(--spacingH))" }}
+              >
                 <TeamPhoto
                   alt={photos[1]?.alt || "Team Photo 2"}
                   src={photos[1]?.src}
                   placeholderText={photos[1]?.placeholderText}
-                  style={{ width: "var(--grid2Width)", height: "var(--grid2Height)" }}
+                  style={{
+                    width: "var(--grid2Width)",
+                    height: "var(--grid2Height)",
+                  }}
                 />
               </div>
 
               {/* Middle row - left image */}
-              <div className="absolute" style={{ top: "calc(var(--grid1Height) + var(--spacingV))", left: "0" }}>
+              <div
+                className="absolute"
+                style={{
+                  top: "calc(var(--grid1Height) + var(--spacingV))",
+                  left: "0",
+                }}
+              >
                 <TeamPhoto
                   alt={photos[2]?.alt || "Team Photo 3"}
                   src={photos[2]?.src}
                   placeholderText={photos[2]?.placeholderText}
-                  style={{ width: "var(--grid3Width)", height: "var(--grid3Height)" }}
+                  style={{
+                    width: "var(--grid3Width)",
+                    height: "var(--grid3Height)",
+                  }}
                 />
               </div>
 
               {/* Middle row - right image */}
-              <div className="absolute" style={{ 
-                top: "calc(var(--grid2Height) + var(--spacingV))", 
-                left: "calc(var(--grid3Width) + var(--spacingH))" 
-              }}>
+              <div
+                className="absolute"
+                style={{
+                  top: "calc(var(--grid2Height) + var(--spacingV))",
+                  left: "calc(var(--grid3Width) + var(--spacingH))",
+                }}
+              >
                 <TeamPhoto
                   alt={photos[3]?.alt || "Team Photo 4"}
                   src={photos[3]?.src}
                   placeholderText={photos[3]?.placeholderText}
-                  style={{ width: "var(--grid4Width)", height: "var(--grid4Height)" }}
+                  style={{
+                    width: "var(--grid4Width)",
+                    height: "var(--grid4Height)",
+                  }}
                 />
               </div>
 
               {/* Bottom row - left image */}
-              <div className="absolute" style={{ 
-                top: "calc(var(--grid1Height) + var(--spacingV) + var(--grid3Height) + var(--spacingV))", 
-                left: "0" 
-              }}>
+              <div
+                className="absolute"
+                style={{
+                  top: "calc(var(--grid1Height) + var(--spacingV) + var(--grid3Height) + var(--spacingV))",
+                  left: "0",
+                }}
+              >
                 <TeamPhoto
                   alt={photos[4]?.alt || "Team Photo 5"}
                   src={photos[4]?.src}
                   placeholderText={photos[4]?.placeholderText}
-                  style={{ width: "var(--grid5Width)", height: "var(--grid5Height)" }}
+                  style={{
+                    width: "var(--grid5Width)",
+                    height: "var(--grid5Height)",
+                  }}
                 />
               </div>
 
               {/* Bottom row - right image - Aligning with the bottom of the grid5 */}
-              <div className="absolute" style={{ 
-                top: "calc(var(--grid1Height) + var(--spacingV) + var(--grid3Height) + var(--spacingV))",
-                left: "calc(var(--grid5Width) + var(--spacingH))"
-              }}>
+              <div
+                className="absolute"
+                style={{
+                  top: "calc(var(--grid1Height) + var(--spacingV) + var(--grid3Height) + var(--spacingV))",
+                  left: "calc(var(--grid5Width) + var(--spacingH))",
+                }}
+              >
                 <TeamPhoto
                   alt={photos[5]?.alt || "Team Photo 6"}
                   src={photos[5]?.src}
                   placeholderText={photos[5]?.placeholderText}
-                  style={{ width: "var(--grid6Width)", height: "var(--grid6Height)" }}
+                  style={{
+                    width: "var(--grid6Width)",
+                    height: "var(--grid6Height)",
+                  }}
                 />
               </div>
 
               {/* Spacer to ensure container maintains proper height */}
-              <div style={{ 
-                height: "calc(var(--grid1Height) + var(--spacingV) + var(--grid3Height) + var(--spacingV) + var(--grid5Height))" 
-              }}></div>
+              <div
+                style={{
+                  height:
+                    "calc(var(--grid1Height) + var(--spacingV) + var(--grid3Height) + var(--spacingV) + var(--grid5Height))",
+                }}
+              ></div>
             </div>
           </div>
         </div>
-        
+
         {/* Right side - Text content */}
         <div className="md:col-span-6 flex flex-col justify-start md:pl-10 lg:pl-16">
           <div className="md:max-w-[45rem]">
-            <Text 
-              type={Font.GARAMOND} 
+            <Text
+              type={Font.GARAMOND}
               className="text-3xl md:text-[2.5rem] lg:text-[2.75rem] font-semibold text-black mb-5 md:leading-[1.2]"
             >
               {title}
             </Text>
-            
+
             <div className="space-y-5">
               {paragraphs.map((paragraph, index) => (
-                <Text 
+                <Text
                   key={index}
-                  type={Font.SOURCE_SANS} 
+                  type={Font.SOURCE_SANS}
                   className="text-base md:text-[1.125rem] lg:text-[1.120rem] text-black/90 md:leading-[1.75]"
                 >
                   {paragraph}
@@ -171,4 +227,4 @@ export default function WhoWeAreSection({ title, paragraphs, photos }: WhoWeAreS
       </div>
     </div>
   );
-} 
+}
