6d88095420ebd2b624cfe53930547843ec5d5ea2
Ansh Arora <ansha4179@gmail.com>
2025-05-30 17:00:26 +0530

    fix: turbopack configuration fix

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,9 @@ const nextConfig: NextConfig = {
   experimental: {
     ppr: true,
   },
+  turbopack: {
+    resolveExtensions: [".mdx", ".tsx", ".ts", ".jsx", ".js", ".mjs", ".json"],
+  },
 };
 
 export default withBundleAnalyzer(nextConfig);
